fd125eb031a2c6004c1ef099b7f3a6200ac7d409
mstrick7 <32557695+mstrick7@users.noreply.github.com>
2019-07-27 10:38:35 -0500

    Initial Commit. Playing with objects in C# in VS.

---
+++ Projects/HelloWorld/HelloWorld/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class Person
+    {
+        String FirstName;
+        String LastName;
+        int a;
+
+        public String Add(String FirstName, String LastName, int a)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.a = a;
+            String name = "Hi I am " + FirstName + " " + LastName+ " and I am "+a+" year's old";
+            return name;
+        }
+       
+
+        //public void Introduce()
+
+        //{
+        //    Console.WriteLine("Hi my name is: " + FirstName + " " + LastName);
+        //    Console.WriteLine("and i am " + a + "years old");
+        //}
+    }
+}
