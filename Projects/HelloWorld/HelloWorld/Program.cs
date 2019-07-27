e45f4f16af81b82ce49bda4aaaf11358469dd0fc
mstrick7 <32557695+mstrick7@users.noreply.github.com>
2019-07-27 11:14:43 -0500

    main method.

---
+++ Projects/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person john = new Person();
+            //john.FirstName = "John";
+            //john.LastName = "Smith";
+
+            //john.Introduce();
+
+            //Person mary = new Person();
+            //mary.FirstName = "Mary";
+            //mary.LastName = "Jane";
+
+            //mary.Introduce();
+
+            Person matt = new Person();
+            String Matt = matt.Add("Matthew", "Strickland", 36);
+
+            //matt.Introduce();
+         
+
+            Person morgan = new Person();
+            String Morgan = morgan.Add("Morgan", "Strickland", 32);
+            
+
+
+            //morgan.Introduce(); 
+
+            Console.WriteLine(Matt);
+            Console.WriteLine(Morgan);
+         
+
+        }
+
+    }
+}
